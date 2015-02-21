dfaaeabcdf1c308bdeddfbe34f5d7eda014a90a4
beck92 <beck_@i.ua>
2015-02-21 15:07:50 +0200

    Bstre Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bstore::Application.config.secret_key_base = 'eb9b65f2be7fd4eef557858af0b27e6298cf1cac69626d5f5c2b653c90dd01db659d8738950de0cb6c5b7df43ba842ef749195f5240ed23d3914daa98be73378'
